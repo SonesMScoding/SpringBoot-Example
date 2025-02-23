1d7b1dcf111e98bf46e5487903f2dda42f71a24c
SonesMScoding <eve.sones@mscoding.org>
2025-02-23 12:50:55 -0600

    fixins

---
+++ taco-cloud/src/main/java/fsa/tacocloud/HomeController.java
@@ -0,0 +1,13 @@
+package fsa.tacocloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
---
+++ taco-cloud/src/test/java/fsa/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package fsa.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
